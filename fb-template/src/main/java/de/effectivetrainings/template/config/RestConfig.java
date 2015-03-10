22afaa93e002e550c8b3453cc58f9670e77054eb
dilgerm <martin@effectivetrainings.de>
2015-03-10 20:51:22 +0100

    provide initial template service

---
+++ fb-template/src/main/java/de/effectivetrainings/template/config/RestConfig.java
@@ -0,0 +1,36 @@
+package de.effectivetrainings.template.config;
+
+import de.effectivetrainings.correlation.CorrelationId;
+import de.effectivetrainings.template.rest.CorrelationIdInterceptor;
+import de.effectivetrainings.spring.metrics.MetricsProvider;
+import de.effectivetrainings.spring.metrics.RestRequestTimerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public CorrelationIdInterceptor correlationIdInterceptor(CorrelationId correlationId) {
+        return new CorrelationIdInterceptor(correlationId);
+    }
+
+    @Bean
+    public RestRequestTimerInterceptor requestTimer(MetricsProvider metricsProvider) {
+        //TODO dilgerm - fix magic string
+        return new RestRequestTimerInterceptor(metricsProvider);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(CorrelationIdInterceptor correlationIdInterceptor, RestRequestTimerInterceptor restRequestTimerInterceptor) {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Arrays.asList(correlationIdInterceptor, restRequestTimerInterceptor));
+        return restTemplate;
+    }
+}

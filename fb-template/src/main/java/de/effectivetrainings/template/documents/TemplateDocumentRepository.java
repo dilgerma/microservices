c817b9a3a2a5593a931db23a4ef219601e65e1c3
dilgerm <martin@effectivetrainings.de>
2015-03-24 23:13:05 +0100

    [eureka] fix dependencies to enable eureka

--- fb-template/src/main/java/de/effectivetrainings/template/documents/TemplateDocumentRepository.java
+++ fb-template/src/main/java/de/effectivetrainings/template/documents/TemplateDocumentRepository.java
@@ -2,14 +2,15 @@ package de.effectivetrainings.template.documents;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
- *
+ * don´t use @Repository here.
+ * see https://github.com/spring-projects/spring-boot/issues/1929
  */
-@Repository
+@Component
 public interface TemplateDocumentRepository extends MongoRepository<TemplateDocument, String> {
 
     @Query("{ 'customerId' : ?0 }")

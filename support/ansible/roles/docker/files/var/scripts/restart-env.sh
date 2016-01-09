18d8a7fcb85635391e800e03b41716f90beb31cc
dilgerm <martin@effectivetrainings.de>
2016-01-09 17:47:18 +0100

    correct influx config for collectd-typesdb

--- support/ansible/roles/docker/files/var/scripts/restart-env.sh
+++ support/ansible/roles/docker/files/var/scripts/restart-env.sh
@@ -1,4 +1,5 @@
-curl -L http://git.io/vEcym -o /usr/local/bin/weave
+
+curl -L https://raw.githubusercontent.com/dilgerma/weave/rpi-latest-release/weave -o /usr/local/bin/weave
 chmod +x /usr/local/bin/weave
 curl -L https://raw.githubusercontent.com/dilgerma/scope/master/scope -o /usr/local/bin/scope
 chmod +x /usr/local/bin/scope
@@ -8,6 +9,6 @@ docker rm -f swarm-agent
 weave reset
 scope stop
 weave launch-proxy -H 0.0.0.0:12375  --no-detect-tls
-weave launch-router --dns-domain billy.local. master-pi pi48 pi46 pi31 pi25 pi24 black-pearl
+weave launch-router --dns-domain billy.local. master-pi pi48 pi46 pi31 pi25 pi24 pi23
 scope launch
 docker -H tcp://0.0.0.0:12375 run --name swarm-agent  -d hypriot/rpi-swarm:latest join --advertise=$IP:12375 token://b261e64ff089835311816d8dea56c58b

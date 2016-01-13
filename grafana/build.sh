4e65961fbaf00ae0612b32ff7e4bdfe53bc25fcb
dilgerm <martin@effectivetrainings.de>
2016-01-13 20:21:42 +0100

    no need to push

--- grafana/build.sh
+++ grafana/build.sh
@@ -4,5 +4,5 @@ if [ -z $TAG]; then
   TAG="latest"
 fi
 docker build --rm -f Dockerfile-rpi -t dilgerm/rpi-grafana:$TAG .
-docker push dilgerm/rpi-grafana:$TAG
+#docker push dilgerm/rpi-grafana:$TAG
 

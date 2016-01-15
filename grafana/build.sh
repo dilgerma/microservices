5128e370130ddd9e56eba63b20f87ac55e0ced5d
dilgerm <martin@effectivetrainings.de>
2016-01-15 20:11:16 +0100

    grafana build

--- grafana/build.sh
+++ grafana/build.sh
@@ -3,6 +3,10 @@ TAG=$1
 if [ -z $TAG]; then
   TAG="latest"
 fi
-docker build --rm -f Dockerfile-rpi -t dilgerm/rpi-grafana:$TAG .
-#docker push dilgerm/rpi-grafana:$TAG
+docker build --rm -f Dockerfile-rpi -t dilgerm/rpi-grafana-build .
+
+container_id=$(docker run -d --name grafana-build -v /gopath1.5/src/github.com/dilgerma/grafana/ dilgerm/rpi-grafana-build)
+docker build -f Dockerfile --volumes-from $container_id -t dilgerm/rpi-grafana:$TAG
+
+
 

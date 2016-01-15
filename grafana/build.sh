098644fe32058ee1b1842f144cc384b71f30a334
Martin Dilger <martin@effectivetrainings.de>
2016-01-15 21:08:26 +0100

    fix

--- grafana/build.sh
+++ grafana/build.sh
@@ -6,10 +6,10 @@ fi
 docker rm -f grafana-build
 docker build --rm -f Dockerfile-rpi -t dilgerm/rpi-grafana-build .
 container_id=$(docker run -d --name grafana-build -v /gopath1.5/src/github.com/dilgerma/grafana/ dilgerm/rpi-grafana-build)
-mkdir build
+echo "removing build file"
+rm -rf build && mkdir build
 cp Dockerfile build
-cd build
-docker cp $container_id:/gopath1.5/src/github.com/dilgerma/grafana/bin/grafana-server/bin .
-docker cp $container_id:/gopath1.5/src/github.com/dilgerma/grafana/public_gen .
-docker build -t dilgerm/rpi-grafana:$TAG
+docker cp $container_id:/gopath1.5/src/github.com/dilgerma/grafana/bin/ build/grafana/bin
+docker cp $container_id:/gopath1.5/src/github.com/dilgerma/grafana/public_gen build/grafana/public_gen
+docker build -t dilgerm/rpi-grafana:$TAG build 
 docker push dilgerm/rpi-grafana:$TAG

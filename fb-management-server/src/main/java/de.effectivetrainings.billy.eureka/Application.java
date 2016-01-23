ca89b4b7db7c374c9cb77bb011bf252c661c00c1
Martin Dilger <martin@effectivetrainings.de>
2016-01-23 22:18:11 +0100

    restructure and simplify services

--- fb-management-server/src/main/java/de.effectivetrainings.billy.eureka/Application.java
+++ fb-management-server/src/main/java/de.effectivetrainings.billy.eureka/Application.java
@@ -1,8 +1,11 @@
-package de.effectivetrainings.billy.metrics;
+package de.effectivetrainings.billy.eureka;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -15,7 +18,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableEurekaServer
 @EnableDiscoveryClient
+@EnableAdminServer
+@EnableEurekaClient
 @EnableTurbine
 @EnableHystrix
 @EnableHystrixDashboard
@@ -24,6 +30,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 
 }
